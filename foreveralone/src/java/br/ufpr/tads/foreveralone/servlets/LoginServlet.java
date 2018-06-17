b80ade6bf3f3bc28e13dd1ce39db451be3dc2aac
gqueiroz <gabriel.queiroz@fh.com.br>
2018-06-17 13:03:32 -0300

    m

--- foreveralone/src/java/br/ufpr/tads/foreveralone/servlets/LoginServlet.java
+++ foreveralone/src/java/br/ufpr/tads/foreveralone/servlets/LoginServlet.java
@@ -67,7 +67,6 @@ public class LoginServlet extends HttpServlet {
                 RequestDispatcher rd = request.
                                 getRequestDispatcher("/paresCompativeis.jsp");
                         request.setAttribute("login", login);
-                        request.setAttribute("msg", "Deu certo");
                         rd.forward(request, response);    
             }
         }
